0890f651d56cd04be48bc2e176db06041f6255e5
Andrew Feeney <webspannerdev@gmail.com>
2016-07-23 08:18:18 +1000

    Initial commit.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = App\User::create([
+            'email'     => 'user@domain.com',
+            'password'  => 'password'
+        ]);
+    }
+}
