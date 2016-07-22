cff0c7357203a2edcc9979953726e5b13a5c2b16
Andrew Feeney <webspannerdev@gmail.com>
2016-07-23 08:28:17 +1000

    Add project model
    - Migration
    - Model

---
+++ database/migrations/2016_07_22_222334_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+
+            // Add columns
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('client_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            // Add foreign key relationships
+            $table->foreign('client_id')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+    }
+}
