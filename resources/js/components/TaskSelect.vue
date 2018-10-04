7cbe0ddeacfe86f7f18c68025d4b9089e25faa95
Andrew Feeney <webspannerdev@gmail.com>
2018-10-04 14:22:11 +1000

    Add input to allow usage as part of html form

--- resources/js/components/TaskSelect.vue
+++ resources/js/components/TaskSelect.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <multi-select v-model="task" label="name" :options="tasks"></multi-select>
+        <multi-select v-model="selectedTask" label="name" :options="tasks"></multi-select>
+        <input type="hidden" name="task_id" :value="taskId">
     </div>
 </template>
 
@@ -10,14 +11,28 @@
 
     export default {
         props: [
-            'tasks'
+            'tasks',
+            'task'
         ],
         components: {
             multiSelect: multiSelect,
         },
         data() {
+            var _this = this;
+
             return {
-                task: null,
+                selectedTask: this.tasks.find(function (task) {
+                    return task.id == _this.task;
+                }),
+            }
+        },
+        computed: {
+            taskId() {
+                if (this.selectedTask == null) {
+                    return null;
+                }
+
+                return this.selectedTask.id;
             }
         }
     }
