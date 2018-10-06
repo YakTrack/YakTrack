4f23f30cc37b8340bee51638daeadbedb66fd934
Andrew Feeney <webspannerdev@gmail.com>
2018-10-07 07:31:45 +1100

    Fix issue where login page returned 500 error

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Auth;
+use Hash;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_guest_can_load_the_login_page()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->get('/login');
+
+        $response->assertSuccessful();
+    }
+
+    /** @test */
+    public function when_a_guest_submits_the_login_form_with_correct_credentials_they_are_logged_in()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create([
+            'email'    => 'test@domain.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $response = $this->post('/login', ['email' => 'test@domain.com', 'password' => 'password']);
+
+        $response->assertRedirect('/');
+
+        $this->assertTrue(Auth::check());
+    }
+}
