1cb1e57b3a0da85f5e444512744e7b3bd4274e7e
Andrew Feeney <webspannerdev@gmail.com>
2018-10-31 16:46:44 +1100

    Fix display of show resource table

--- resources/views/layouts/show-resource-table.blade.php
+++ resources/views/layouts/show-resource-table.blade.php
@@ -1,10 +1,12 @@
-<table class="table table-borderless table-hover show-resource-table">
-    <tbody>
-        @foreach($resource as $key => $value)
-            <tr>
-                <td><span class="h5"> {{ $key }} </span></td>
-                <td><span class="h5"> {{ $value }} </span></td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="rounded p-2 bg-white shadow mb-8">
+    <table class="table rounded p-4 bg-white">
+        <tbody>
+            @foreach($resource as $key => $value)
+                <tr>
+                    <td><span class="text-grey-dark p-2"> {{ $key }} </span></td>
+                    <td><span class="text-lg leading-loose p-2"> {{ $value }} </span></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
