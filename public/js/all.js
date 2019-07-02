69d8593e6da75c9229e21b429202d99936407788
Andrew Feeney <webspannerdev@gmail.com>
2019-07-02 11:50:32 +1000

    Rebuild assets

--- public/js/all.js
+++ public/js/all.js
@@ -3024,7 +3024,7 @@ $(document).ready(function () {
     // Get item information from DOM
     var itemTypeContainer = $(this).closest('.item-type-container');
     var itemContainer = $(this).closest('.item-container');
-    console.log($(itemTypeContainer).data('item-type'));
+
     // Load information into delete item model
     $('.item-name').text($(itemContainer).data('item-name'));
     $('.item-type').text($(itemTypeContainer).data('item-type'));
