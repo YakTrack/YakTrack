e01855d87457bc20cead186fcc0ac944b87b5965
Andrew Feeney <webspannerdev@gmail.com>
2019-12-28 15:41:15 +1100

    Implement project select component

---
+++ resources/js/Shared/ProjectSelect.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <multi-select
+            v-model="selectedProject"
+            label="name"
+            track-by="id"
+            :options="projects"
+        ></multi-select>
+    </div>
+</template>
+
+<script>
+
+    import multiSelect from 'vue-multiselect';
+
+    export default {
+        props: [
+            'projects',
+            'value',
+        ],
+        components: {
+            multiSelect: multiSelect,
+        },
+        data() {
+            return {
+                selectedProject: this.projects.find(
+                    project => project.id == this.value
+                ),
+            };
+        },
+        watch: {
+            selectedProject(value) {
+                this.$emit('input', value ? value.id : null);
+            }
+        }
+    }
+
+</script>
