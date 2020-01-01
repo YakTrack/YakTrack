3a7e2641a3e76dc6f90774a5e36151f831f94b7f
Andrew Feeney <webspannerdev@gmail.com>
2020-01-01 13:51:31 +1100

    Improve task factory

--- app/Support/FactoryGenerator.php
+++ app/Support/FactoryGenerator.php
@@ -8,7 +8,16 @@ class FactoryGenerator
     {
         return collect([
             $this->projectVerb(),
-            $this->projectNounAndArticle(),
+            $this->nounAndArticle(),
+        ])->implode(' ');
+    }
+
+    public function taskName()
+    {
+        return collect([
+            $this->storyUser(),
+            $this->taskVerb(),
+            $this->nounAndArticle(),
         ])->implode(' ');
     }
 
@@ -32,7 +41,33 @@ class FactoryGenerator
         ])->random();
     }
 
-    protected function projectNounAndArticle()
+    protected function taskVerb()
+    {
+        return collect([
+            'click',
+            'update',
+            'view',
+            'edit',
+            'update',
+            'drag',
+            'search through',
+            'filter',
+            'delete',
+            'deactivate',
+            'activate',
+            'enable',
+            'disable',
+            'exit',
+            'login to',
+            'logout of',
+            'switch to',
+            'switch from',
+            'navigate to',
+            'navigate away from',
+        ])->random();
+    }
+
+    protected function nounAndArticle()
     {
         $isPlural = array_random([true, false]);
         $noun = $isPlural ? $this->pluralNoun() : $this->noun();
@@ -104,5 +139,16 @@ class FactoryGenerator
     protected function pluralNoun()
     {
         return str_plural($this->noun());
+    }
+
+    protected function storyUser()
+    {
+        return collect([
+            'as a user I can',
+            'as an admin I can',
+            'as a guest I can',
+            'as a vendor I can',
+            'as a subscriber I can',
+        ])->random();
     }
 }
--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,12 +2,13 @@
 
 use App\Models\Project;
 use App\Models\Task;
+use App\Support\FactoryGenerator;
 use Faker\Generator as Faker;
 
 $factory->define(Task::class, function (Faker $faker) {
     return [
-        'name'        => $faker->sentence,
-        'description' => $faker->sentence,
+        'name'          => ucfirst(app(FactoryGenerator::class)->taskName()),
+        'description' => $faker->paragraph,
         'status'      => $faker->word,
         'project_id' => function () {
             return factory(Project::class)->create()->id;
