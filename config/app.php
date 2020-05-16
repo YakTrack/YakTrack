70f7b808a82f5c8358e2147effc680a1c37e4da9
Andrew Feeney <andrew@deskspacecreative.com.au>
2020-05-16 17:45:43 +1000

    Use auto discovery

--- config/app.php
+++ config/app.php
@@ -171,7 +171,6 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
         /*
