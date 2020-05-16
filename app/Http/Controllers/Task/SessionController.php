9ecf0bd62d45e5f438cbff62e44fa7f298b6291f
Andrew Feeney <andrew@deskspacecreative.com.au>
2020-05-17 07:55:35 +1000

    Fix date format issues

--- app/Http/Controllers/Task/SessionController.php
+++ app/Http/Controllers/Task/SessionController.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers\Task;
 use App\Http\Controllers\Controller;
 use App\Models\Session;
 use App\Models\Task;
+use App\Support\DateTimeFormatter;
 use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
+    protected $dateTimeFormatter;
+
+    public function __construct(DateTimeFormatter $dateTimeFormatter)
+    {
+        $this->dateTimeFormatter = $dateTimeFormatter;
+    }
+
     public function store(Task $task)
     {
         Session::running()->get()->each(function ($session) {
@@ -16,8 +24,8 @@ class SessionController extends Controller
         });
 
         $session = $task->sessions()->create([
-            'started_at' => request('started_at'),
-            'ended_at'   => request('ended_at'),
+            'started_at' => $this->dateTimeFormatter->utcFormat(request('started_at')),
+            'ended_at'   => request('ended_at') ? $this->dateTimeFormatter->utcFormat(request('ended_at')) : null,
             'sprint_id'  => $task->openSprint()->id,
         ]);
 
