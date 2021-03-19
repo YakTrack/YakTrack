084fae2ae9316322057a011ea8ca941ef6dfd9cd
Andrew Feeney <WebSpanner@users.noreply.github.com>
2021-03-19 03:04:02 +0000

    Apply fixes from StyleCI

--- tests/Feature/Session/EditSessionsTest.php
+++ tests/Feature/Session/EditSessionsTest.php
@@ -2,12 +2,7 @@
 
 namespace Tests\Feature\Session;
 
-use App\Models\Invoice;
 use App\Models\Session;
-use App\Models\SessionCategory;
-use App\Models\Sprint;
-use App\Models\Task;
-use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -27,7 +22,7 @@ class EditSessionsTest extends TestCase
         $this->actingAsUser();
 
         $response = $this->patch(
-            route('sessions.update'), 
+            route('sessions.update'),
             [
                 'sessions' => [
                     $sessions[0]->id => [
@@ -36,19 +31,19 @@ class EditSessionsTest extends TestCase
                     $sessions[1]->id => [
                         'is_billable' => 1,
                     ],
-                ]
+                ],
             ]
         );
 
         $response->assertSuccessful();
 
         $this->assertDatabaseHas('sessions', [
-            'id' => $sessions[0]->id,
+            'id'          => $sessions[0]->id,
             'is_billable' => 1,
         ]);
 
         $this->assertDatabaseHas('sessions', [
-            'id' => $sessions[1]->id,
+            'id'          => $sessions[1]->id,
             'is_billable' => 1,
         ]);
     }
