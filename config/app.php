fba734817de659a05474cff72b1fc95cc39ef69a
Andrew Feeney <andrew@deskspacecreative.com.au>
2025-01-15 20:19:14 +1100

    Update to Laravel 8 and require PHP 8

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         /*
          * Package Service Providers
          **/
-        Collective\Html\HtmlServiceProvider::class,
         Spatie\BladeJavaScript\BladeJavaScriptServiceProvider::class,
     ],
     /*
@@ -203,10 +202,8 @@ return [
         'Eloquent'     => Illuminate\Database\Eloquent\Model::class,
         'Event'        => Illuminate\Support\Facades\Event::class,
         'File'         => Illuminate\Support\Facades\File::class,
-        'Form'         => Collective\Html\FormFacade::class,
         'Gate'         => Illuminate\Support\Facades\Gate::class,
         'Hash'         => Illuminate\Support\Facades\Hash::class,
-        'Html'         => Collective\Html\HtmlFacade::class,
         'Lang'         => Illuminate\Support\Facades\Lang::class,
         'Log'          => Illuminate\Support\Facades\Log::class,
         'Mail'         => Illuminate\Support\Facades\Mail::class,
