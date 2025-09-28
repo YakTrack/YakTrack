75cbcd71392872ff55864eeecfccca02cdd4740d
Andrew Feeney <andrew@deskspacecreative.com.au>
2025-09-28 14:08:16 +1000

    Upgrade to Laravel 11

--- database/migrations/2020_09_19_213150_create_targets_table.php
+++ database/migrations/2020_09_19_213150_create_targets_table.php
@@ -21,7 +21,7 @@ class CreateTargetsTable extends Migration
             $table->string('duration_unit')->nullable();
             $table->unsignedInteger('duration')->nullable();
             $table->string('value_unit');
-            $table->unsignedDecimal('value');
+            $table->decimal('value')->unsigned();
             $table->boolean('billable_only');
             $table->timestamps();
         });
