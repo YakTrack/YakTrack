bb5f727767cdec1e644a86b8e576d68e7ac13a69
Andrew Feeney <andrew@deskspacecreative.com.au>
2025-10-20 05:01:46 +1100

    Improve dropdown in sessions table

--- resources/js/Shared/ActionsDropdown.vue
+++ resources/js/Shared/ActionsDropdown.vue
@@ -1,58 +1,54 @@
 <template>
-    <div class="relative text-right" v-closeable="{
-        exclude: ['button'],
-        handler: 'onClose'
-    }">
-        <button class="p-2 text-gray-400 hover:text-gray-600 transition-colors duration-150" @click="toggleIsOpen()">
+    <Menu as="div" class="relative inline-block">
+        <MenuButton class="p-2 text-gray-400 hover:text-gray-600 transition-colors duration-150">
             <i class="fas fa-ellipsis-v"></i>
-        </button>
-        <div class="rounded shadow-md absolute top-full right-0 mt-1 min-w-full bg-white text-left z-10" :class="[dropdownClasses, isOpen || 'dropdown-closed']">
-            <ul class="list-reset w-max-content min-w-full p-1">
-                <li v-for="option in options" :key="option.name" class="clickable"> 
-                    <a
-                        v-on:click="optionWasClicked(option)"
-                        class="px-4 py-2 block text-gray-700 font-normal hover:bg-grey-300 no-underline"
-                    > {{ option.name }} </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+        </MenuButton>
+
+        <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform scale-100" leave-to-class="transform opacity-0 scale-95">
+            <MenuItems :class="[dropdownClasses, 'absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg outline-1 outline-black/5 dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10']">
+                <div class="py-1">
+                    <MenuItem v-for="option in options" :key="option.name" v-slot="{ active }">
+                        <a
+                            href="#"
+                            @click.prevent="optionWasClicked(option)"
+                            :class="[active ? 'bg-gray-100 text-gray-900 outline-hidden dark:bg-white/5 dark:text-white' : 'text-gray-700 dark:text-gray-300', 'block px-4 py-2 text-sm text-left']"
+                        >
+                            {{ option.name }}
+                        </a>
+                    </MenuItem>
+                </div>
+            </MenuItems>
+        </transition>
+    </Menu>
 </template>
 
-<script>
-export default {
-    props: [
-        'options',
-        'direction',
-    ],
-    data() {
-        return {
-            isOpen: false,
-        }
+<script setup>
+import { computed } from 'vue'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+
+const props = defineProps({
+    options: {
+        type: Array,
+        required: true
     },
-    methods: {
-        optionWasClicked(option) {
-            if (option.callback) {
-                option.callback();
-            } else if (option.event) {
-                typeof option.event == 'string' ? events.emit(option.event) : events.emit(option.event.name, option.event.args);
-            }
-            this.isOpen = false;
-        },
-        toggleIsOpen() {
-            this.isOpen = !this.isOpen;
-        },
-        onClose() {
-            this.isOpen = false;
-        }
-    },
-    computed: {
-        dropdownClasses() {
-            if (this.direction === 'left') {
-                return 'right-0';
-            }
-            return 'left-0';
-        }
-    },
+    direction: {
+        type: String,
+        default: 'right'
+    }
+})
+
+const optionWasClicked = (option) => {
+    if (option.callback) {
+        option.callback();
+    } else if (option.event) {
+        typeof option.event == 'string' ? events.emit(option.event) : events.emit(option.event.name, option.event.args);
+    }
 }
+
+const dropdownClasses = computed(() => {
+    if (props.direction === 'left') {
+        return 'right-0';
+    }
+    return 'left-0';
+})
 </script>
--- resources/js/Shared/Dropdown.vue
+++ resources/js/Shared/Dropdown.vue
@@ -1,62 +1,64 @@
 <template>
-    <div class="relative text-right shadow rounded p-2" v-closeable="{
-        exclude: ['button'],
-        handler: 'onClose'
-    }">
-        <button class="" @click="toggleIsOpen()">
-            <span> {{ label || 'Actions' }} </span>
-            <i class="fas fa-caret-down"></i>
-        </button>
-        <div class="rounded shadow-md mt-2 absolute mt-12 mb-12 top-0 min-w-full bg-white text-left z-10" :class="[dropdownClasses, isOpen || 'dropdown-closed']">
-            <ul class="list-reset w-max-content min-w-full p-1">
-                <li v-for="option in options" :key="option.name" class="clickable"> 
-                    <a
-                        v-on:click="optionWasClicked(option)"
-                        class="px-4 py-2 block text-gray-700 font-normal hover:bg-grey-300 no-underline"
-                    > {{ option.name }} </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <Menu as="div" class="relative inline-block">
+        <MenuButton class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs inset-ring-1 inset-ring-gray-300 hover:bg-gray-50 dark:bg-white/10 dark:text-white dark:shadow-none dark:inset-ring-white/5 dark:hover:bg-white/20">
+            {{ label || 'Actions' }}
+            <ChevronDownIcon class="-mr-1 size-5 text-gray-400" aria-hidden="true" />
+        </MenuButton>
+
+        <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform scale-100" leave-to-class="transform opacity-0 scale-95">
+            <MenuItems :class="[dropdownClasses, 'absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg outline-1 outline-black/5 dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10']">
+                <div class="py-1">
+                    <MenuItem v-for="option in options" :key="option.name" v-slot="{ active }">
+                        <a
+                            href="#"
+                            @click.prevent="optionWasClicked(option)"
+                            :class="[active ? 'text-left bg-gray-100 text-gray-900 outline-hidden dark:bg-white/5 dark:text-white' : 'text-gray-700 dark:text-gray-300', 'block px-4 py-2 text-sm text-left']"
+                        >
+                            {{ option.name }}
+                        </a>
+                    </MenuItem>
+                </div>
+            </MenuItems>
+        </transition>
+    </Menu>
 </template>
 
-<script>
-export default {
-    props: [
-        'options',
-        'name',
-        'selected-option',
-        'direction',
-    ],
-    data() {
-        return {
-            isOpen: false,
-            selected: this.selectedOption,
-        }
+<script setup>
+import { computed } from 'vue'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+
+const props = defineProps({
+    options: {
+        type: Array,
+        required: true
     },
-    methods: {
-        optionWasClicked(option) {
-            typeof option.event == 'string' ? events.emit(option.event) : events.emit(option.event.name, option.event.args);
-            this.selected = option;
-            this.isOpen = false;
-        },
-        toggleIsOpen() {
-            this.isOpen = !this.isOpen;
-        },
-        onClose() {
-            this.isOpen = false;
-        }
+    name: {
+        type: String,
+        default: null
     },
-    computed: {
-        label() {
-            return this.name;
-        },
-        dropdownClasses() {
-            if (this.direction === 'left') {
-                return 'right-0';
-            }
-            return 'left-0 -ml-1';
-        }
+    selectedOption: {
+        type: Object,
+        default: null
     },
+    direction: {
+        type: String,
+        default: 'right'
+    }
+})
+
+const optionWasClicked = (option) => {
+    typeof option.event == 'string' ? events.emit(option.event) : events.emit(option.event.name, option.event.args);
 }
+
+const label = computed(() => {
+    return props.name;
+})
+
+const dropdownClasses = computed(() => {
+    if (props.direction === 'left') {
+        return 'right-0';
+    }
+    return 'left-0 -ml-1';
+})
 </script>
